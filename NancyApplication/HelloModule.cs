e7d7c813755fbda20a13a61304317f58b860912d
mnasif786 <muhammadnauman.asif@pbs44143.hq.peninsula-uk.local>
2015-05-06 11:35:05 +0100

    nancy proj
    nancy proj

---
+++ NancyApplication/HelloModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancyApplication
+{
+    public class HelloModule : NancyModule 
+    {
+        public HelloModule()
+        {
+            Get["/hello"] = parameters => "Hello World";
+        }
+    }
+}
---
+++ NancyApplication/IndexModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.UI.WebControls;
+
+namespace NancyApplication
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters => View["index"];
+        }
+    }
+}
